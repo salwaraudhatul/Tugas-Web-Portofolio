59d2c58ff73a4725359e033d7556a2a56c4fba28
salwaraudhatul <salwaraudhatul330@gmail.com>
2025-06-19 19:34:16 +0700

    ss hero

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
-//Toggle class active
-const navbarNav = document.querySelector (".navbar-nav");
-// ketika hamburger menu di klik
+//toggle class active
+
+const navbarNav = document.querySelector(".navbar-nav");
+
+//hamburger menu responsif
 document.querySelector("#hamburger-menu").onclick = () => {
-    navbarNav.classList.toggle("active");
+  navbarNav.classList.toggle("active");
 };
+
+//klik diluar sidebar buat ilangin nav
+
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
